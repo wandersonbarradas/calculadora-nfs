96a061c846e8abfd1cbb7bd7eb0e354e71b7bfe0
Wanderson Barradas <wandersonbarradas07@gmail.com>
2023-06-20 17:14:04 -0300

    - teste 3

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -2,12 +2,12 @@ import { Calculator } from "@/components/Calculator";
 import Api from "@/libs/api";
 
 export default async function Home() {
-    // const data = await Api.getIrrfTable();
+    const data = await Api.getIrrfTable();
     return (
         <>
             <div className="mt-8">
                 <h1 className="text-2xl">Calculo de imposto</h1>
-                <Calculator dataIrrf={[]} />
+                <Calculator dataIrrf={data} />
             </div>
         </>
     );
